d6baee2c09ee2ee8d0d207ab53392e8e0096e543
Rickson <ricksonrocha0@gmail.com>
2023-08-19 18:37:32 -0300

    :sparkles: feat: adição da calculadora de média

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, } from 'react-native';
 
 export default function App() {
+  const [value1, setValue1] = React.useState(0)
+  const [value2, setValue2] = React.useState(0)
+  const [result, setResult] = React.useState(0)
+
+  React.useEffect(() => {
+    setResult(value1 + value2)
+  }, [value1, value2])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Calculadora de média</Text>
+      <TextInput style={styles.input} keyboardType='numeric' placeholder="Nota 1" value={String(value1)} onChangeText={(e) => setValue1(Number(e))}/>
+      <TextInput style={styles.input} keyboardType='numeric'  placeholder="Nota 2" value={String(value2)} onChangeText={(e) => setValue2(Number(e))}/>
+      <Text style={styles.result(result)} >Resultado: {result}</Text>
+      <Text style={styles.footer}>GRR20210545</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +30,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  input: {
+    borderWidth: 1,
+    marginTop:10,
+    width: '50%',
+    borderStyle: 'solid',
+    textAlign:'center'
+  },
+  result: (r) => {
+    let color = 'red'
+    if (r >= 70) {
+      color = 'green'
+    } else if (r >= 40) {
+      color = 'orange'
+    }
+    return {
+      marginTop: 25,
+      color,
+      fontSize: 20,
+    }
+  },
+  footer: {
+    marginTop: 5,
+    fontSize: 15,
+  }
 });
